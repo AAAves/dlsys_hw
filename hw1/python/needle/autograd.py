aba9ee8620a13a21b1de9721a243af4ee6464157
AAAves <ylzaves@163.com>
2023-06-18 01:28:39 +0800

    hw1-q3

--- hw1/python/needle/autograd.py
+++ hw1/python/needle/autograd.py
@@ -411,14 +411,24 @@ def find_topo_sort(node_list: List[Value]) -> List[Value]:
     sort.
     """
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    visited = []
+    topo_order = []
+    for node in node_list:
+        if node not in visited:
+            topo_sort_dfs(node,visited,topo_order)
+    return topo_order
     ### END YOUR SOLUTION
 
 
 def topo_sort_dfs(node, visited, topo_order):
     """Post-order DFS"""
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    visited.append(node)
+    if len(node.inputs) != 0:
+        for input_node in node.inputs:
+            if input_node not in visited:
+                topo_sort_dfs(input_node, visited, topo_order)
+    topo_order.append(node)
     ### END YOUR SOLUTION
 
 
